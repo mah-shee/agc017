ba03d50d88fbe24d19ad870f7336082ce8a53e89
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-19 21:30:57 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        p: usize,
+        a: [usize; n],
+    }
+    let mut count_even = 0;
+    for i in a.iter() {
+        if i % 2 == 0 {
+            count_even += 1;
+        }
+    }
+    if count_even == n && p == 0 {
+        println!("{}", 2usize.pow(n as u32));
+    } else if count_even == n && p == 1 {
+        println!("0");
+    } else {
+        println!("{}", 2usize.pow((n - 1) as u32));
     }
 }
